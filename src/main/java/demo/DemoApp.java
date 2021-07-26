ce2898ed7fcc5ec8b872418294ef78d21f1077ab
roll57 <rollinger.thomas@gmail.com>
2021-07-26 09:03:03 +0200

    Add files via upload

---
+++ src/main/java/demo/DemoApp.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+}
